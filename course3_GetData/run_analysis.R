f8d52c5ad6717d2412644a9ab918f5d40ec934ce
Paul Roetman <proetman@gmail.com>
2017-07-02 08:59:13 +1000

    finalize course 3

--- course3_GetData/run_analysis.R
+++ course3_GetData/run_analysis.R
@@ -1,4 +1,13 @@
+# Program: Run Analyse
+#    This program will load the training and test data sets,
+#    merge the data into a single set,
+#    then print the average for each subject/activity for each measure.
 
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+#
+#                        Load Libraries
+#
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 library(dplyr)
 
@@ -9,51 +18,76 @@ library(dplyr)
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 dir_proj_data <- 'C:/temp/UCI HAR Dataset'
-tmp_dir <- 'c:/temp'
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 #
-#                    Set Default Working Directory
+#                    Tidy Column Names
 #
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-# Change directory to the global working directory defined for this project.
+# Tidy up column names by
+#    removing abbreviations
+#    init cap words to make it readable
+#    remove column numbers from headings.
 
-dir_set_default_working <- function(){
-        # Change to the default working directory for
-        # this weeks project/assignment.
-        #
-        setwd(dir_proj_working)
+tidy_column_names <- function(p_data){
+
+        # Note: the order of these substitutions is important, for example
+        #    Freq -> Frequency must go before
+        #    f -> Frequency; otherwise the substituion would occur twice.
+        names(p_data) <- gsub('activity_full','Activity',names(p_data))
+        names(p_data) <- gsub('Acc','Acceleration',names(p_data))
+        names(p_data) <- gsub('Gyro','Gyroscope',names(p_data))
+        names(p_data) <- gsub('_t','_Time',names(p_data))
+        names(p_data) <- gsub('Freq','Frequency',names(p_data))
+        names(p_data) <- gsub('_f','_Frequency',names(p_data))
+        names(p_data) <- gsub('tBody','TimeBody',names(p_data))
+        names(p_data) <- gsub('mean','Mean',names(p_data))
+        names(p_data) <- gsub('std','STD',names(p_data))
+        names(p_data) <- gsub('angle','Angle',names(p_data))
+        names(p_data) <- gsub('BodyBody','body',names(p_data))
+        names(p_data) <- gsub('Mag','magnitude',names(p_data))
+        names(p_data) <- gsub('gravity','Gravity',names(p_data))
+
+        # The column numbers are no longer requred as duplicate column
+        # names have been remvoed.
+        names(p_data) <- gsub('^[0-9]*_','', names(p_data))
+
+        # print(names(p_data))    # debug only
+
+        # Return the data with clean and tidy column names.
+        return(p_data)
+
 }
 
-
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 #
-#                    Set Default Working Directory
+#                    Load data
 #
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-# Change directory to the global working directory defined for this project.
+# Load the measurement data
+#    Assign the subject and activity to each line of data
+#    Assign column headings
 
 load_data <- function(p_dir, p_activity_labels, p_col_all, p_col_required){
 
         mode <- basename(p_dir)
+        print(paste0('load data from: ', mode))
 
         subject_file <- paste(paste("subject", mode, sep="_"), 'txt', sep='.')
         x_file <- paste(paste("x", mode, sep="_"), 'txt', sep='.')
         y_file <- paste(paste("y", mode, sep="_"), 'txt', sep='.')
 
         #  load subject test (who)
-        print(paste0('load subject file: ', subject_file))
         subjects_file <- paste(p_dir, subject_file, sep='/')
         subjects <- read.delim(subjects_file, header=FALSE)
         names(subjects) <- c('Subject')
 
         #  load Y (what activity)
-        print(paste0('load Y file      : ', y_file))
+
         activities_file <- paste(p_dir, y_file, sep='/')
         activities <- read.delim(activities_file, header=FALSE)
 
         #  load data (results)
-        print(paste0('load X file      : ', x_file))
         field_widths <- rep(16, 561)
         results_file <- paste(p_dir, x_file, sep='/')
         results <- read.fwf(results_file, field_widths)
@@ -66,8 +100,6 @@ load_data <- function(p_dir, p_activity_labels, p_col_all, p_col_required){
 
         # Add who did what
         results <- cbind(subjects, results)
-        print(names(results))
-        # results$who <- subjects
 
         # Add there activity
         results$activity <- activities
@@ -80,9 +112,10 @@ load_data <- function(p_dir, p_activity_labels, p_col_all, p_col_required){
                                        2,
                                        function(x) map[as.character(x)])
 
-        names(results)
+        # remove the acvitiy code, only require activity name!
         results <- select(results, -activity)
-        names(results)
+
+        # Return the dataset.
         return(results)
 
 
@@ -94,6 +127,9 @@ load_data <- function(p_dir, p_activity_labels, p_col_all, p_col_required){
 #
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # Load the activity lookup
+#    Has 2 columns
+#    ID:   Activity ID, data range 1-6
+#    Name: Activity Name
 
 load_activity_lookup <- function(p_dir, p_file) {
 
@@ -102,7 +138,9 @@ load_activity_lookup <- function(p_dir, p_file) {
         names(activity_labels) <- c('activityid','activityname')
 
         # remove leading and trailing spaces
-        activity_labels$activityname <- gsub(" ","", activity_labels$activityname)
+        activity_labels$activityname <- gsub(" ",
+                                             "",
+                                             activity_labels$activityname)
 
         return(activity_labels)
 }
@@ -112,7 +150,9 @@ load_activity_lookup <- function(p_dir, p_file) {
 #                    Load Column Headings
 #
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-# Load the column headings
+# Load the column headings from the features file.
+#   Pre-pend the column number to the name so when non-alpha characters
+#   are removed, duplicate columns are not created.
 
 load_column_headings <- function(p_dir, p_file) {
         data_file <- paste(p_dir, p_file, sep='/')
@@ -133,6 +173,12 @@ load_column_headings <- function(p_dir, p_file) {
 #
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # Run Analysis
+#    Load the column headings and activity labels,
+#    then load the train and test data.
+#    Merge the training and test data to a single data frame, and
+#    remove columns that are not required.
+#    Then generate the mean for each variable per subject per activity.
+#    Save this data to result.txt file.
 
 run_analysis <- function() {
 
@@ -165,11 +211,21 @@ run_analysis <- function() {
 
         merged_data <- rbind(train_data, test_data)
 
-        myresult <- merged_data %>%
-                    group_by(Subject, activity_full) %>%
-                    summarise_each(funs(mean(.)))
+        merged_data <- tidy_column_names(merged_data)
 
-        print(dim(myresult))
+        result_data <- merged_data %>%
+                       group_by(Subject, Activity) %>%
+                       summarise_each(funs(mean(.)))
 
+        names(result_data)[-c(1:2)] <- paste0(names(result_data)[-c(1:2)],
+                                              '_Mean')
 
+        print('write data to results.txt')
+        write.table(result_data,
+                    file=paste(dir_proj_data,
+                               'results.txt',
+                               sep='/'),
+                    row.names = FALSE)
+
+        return(TRUE)
 }
